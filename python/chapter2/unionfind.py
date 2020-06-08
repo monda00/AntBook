d8ace58491d61da83f0df573e782fd308ce9ae2d
monda00 <monda0524@gmail.com>
2020-06-08 19:54:11 +0900

    add unionfind

---
+++ python/chapter2/unionfind.py
@@ -0,0 +1,107 @@
+class UnionFind():
+    """
+    Union Find木クラス
+
+    Attributes
+    --------------------
+    n : int
+        要素数
+    root : list
+        木の要素数
+        0未満であればそのノードが根であり、添字の値が要素数
+    rank : list
+        木の深さ
+    """
+
+    def __init__(self, n):
+        """
+        Parameters
+        ---------------------
+        n : int
+            要素数
+        """
+        self.n = n
+        self.root = [-1]*(n+1)
+        self.rank = [0]*(n+1)
+
+    def Find_Root(self, x):
+        """
+        ノードxの根を見つける
+
+        Parameters
+        ---------------------
+        x : int
+            見つけるノード
+
+        Returns
+        ---------------------
+        root : int
+            根のノード
+        """
+        if(self.root[x] < 0):
+            return x
+        else:
+            self.root[x] = self.Find_Root(self.root[x])
+            return self.root[x]
+
+    def Unite(self, x, y):
+        """
+        木の併合
+
+        Parameters
+        ---------------------
+        x : int
+            併合したノード
+        y : int
+            併合したノード
+        """
+        # 入力ノードのrootノードを見つける
+        x = self.Find_Root(x)
+        y = self.Find_Root(y)
+        # すでに同じ木に属していた場合
+        if(x == y):
+            return
+        # 違う木に属していた場合rankを見てくっつける方を決める
+        elif(self.rank[x] > self.rnk[y]):
+            self.root[x] += self.root[y]
+            self.root[y] = x
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            # rankが同じ（深さに差がない場合）は1増やす
+            if(self.rank[x] == self.rnk[y]):
+                self.rank[y] += 1
+
+    def isSameGroup(self, x, y):
+        """
+        同じグループに属するか判定
+
+        Parameters
+        ---------------------
+        x : int
+            判定したノード
+        y : int
+            判定したノード
+
+        Returns
+        ---------------------
+        ans : bool
+            同じグループに属しているか
+        """
+        return self.Find_Root(x) == self.Find_Root(y)
+
+    def Count(self, x):
+        """
+        木のサイズを計算
+
+        Parameters
+        ---------------------
+        x : int
+            計算したい木のノード
+
+        Returns
+        ---------------------
+        cnt : int
+            木のサイズ
+        """
+        return -self.root[self.Find_Root(x)]
